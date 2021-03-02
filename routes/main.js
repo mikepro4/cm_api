b08149b8c945b81a27a78e2ee6fda04e0bb0faf8
Mikhail <pmp@nopreset.ru>
2021-03-02 02:45:41 -0500

    count of collections

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 
+const mongoose = require("mongoose");
+const Proxy = mongoose.model("proxies");
+const Ticker = mongoose.model("tickers");
+
 module.exports = app => {
 	app.get(
         "/user_details",
@@ -9,5 +13,44 @@ module.exports = app => {
             res.send(req.user)
 		}
 	);
+
+	// ===========================================================================
+
+	app.get("/collections/counts", async (req, res) => {
+		const { criteria, sortProperty, offset, limit, order } = req.body;
+		let adjustSortProperty 
+		if (sortProperty == "createdAt") {
+			adjustSortProperty = sortProperty
+		} else {
+			adjustSortProperty = "metadata." + sortProperty
+		}
+		const queryProxies = Proxy.find(buildQuery(criteria))
+			.sort({ [adjustSortProperty]: order })
+			.skip(offset)
+			.limit(limit);
+
+		const queryTickers = Ticker.find(buildQuery(criteria))
+			.sort({ [adjustSortProperty]: order })
+			.skip(offset)
+			.limit(limit);
+
+		return Promise.all(
+			[queryProxies.countDocuments(), queryTickers.countDocuments()]
+		).then(
+			results => {
+				return res.json({
+					proxies: results[0],
+					tickers: results[1],
+				});
+			}
+		);
+	});
 };
+
+const buildQuery = criteria => {
+	const query = {};
+
+	return query
+};
+
 
