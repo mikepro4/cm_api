352f709b1d6598765be9e0305a606da317907b95
Mikhail Proniushkin <pmp@nopreset.ru>
2021-05-10 18:07:11 -0400

    back to original proxies

--- scraping/search_results/scrape_yt_search.js
+++ scraping/search_results/scrape_yt_search.js
@@ -131,8 +131,8 @@ function load(query, options, proxy) {
     return new Promise((resolve, reject) => {
         request({
             // url: url,
-            // proxy: "http://" + proxy,
-            url: "http://tickerrrproxy1.herokuapp.com/" + url,
+            proxy: "http://" + proxy,
+            // url: "http://tickerrrproxy1.herokuapp.com/" + url,
             // proxy: proxy,
             headers: {
                 'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36.'
